21cf48ca83df026b0074b42188a710333c776366
KamalKant786 <82696095+KamalKant786@users.noreply.github.com>
2023-05-16 02:09:23 +0530

    Update multiple_disease_prediction.py

--- multiple_disease_prediction.py
+++ multiple_disease_prediction.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 #loading the saved models
 
-diabetes_model = pickle.load(open('/Users/princesharma/Downloads/ete project/Saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('/Users/princesharma/Downloads/ete project/Saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('/Users/princesharma/Downloads/ete project/Saved models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
 
 # side bar for navigation
